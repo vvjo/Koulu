f781fcce5520c2a2254489dd92a71c9633c17234
Väinö Kojo <vaino.kojo@metropolia.fi>
2020-04-06 13:35:19 +0300

    sm t8

---
+++ suunnittelumallit/t8/src/KSP.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+
+public class KSP extends Game {
+	
+	private Scanner scanner = new Scanner(System. in);
+	private int goal;
+	private int arvaus;
+	private int player;
+	
+	protected void initializeGame() {
+		System.out.println("Arvaa numero 1-20");
+		goal = (int)(Math.random() * 20) + 1;
+		System.out.println("goal "+goal);
+		
+	}
+
+	protected void makePlay(int player) {
+		System.out.println("Pelaaja "+(player+1)+" arvaa");
+		this.player = player;
+		arvaus = Integer.parseInt(scanner.nextLine());
+	}
+
+	protected boolean endOfGame() {
+		if(arvaus==0) return false;
+		if(arvaus==goal) {
+			return true;
+		}else {
+			System.out.println("Väärin. Seuraavan pelaajan vuoro");
+			if(arvaus>goal) {
+				System.out.println("Haettu luku on pienempi");
+			}else {
+				System.out.println("Haettu luku on suurempi");
+			}
+			return false;
+		}
+	}
+
+	protected void printWinner() {
+		System.out.println("Pelaaja "+(player+1)+" voitti");
+	}
+
+}
